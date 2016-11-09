6ef7dd135a6ce5fa99456d65663d19933a3b5018
Myza Taras <taras.myza@gmail.com>
2016-11-09 15:46:22 +0200

    First commit

--- src/Massive2.java
+++ src/Massive2.java
@@ -4,29 +4,29 @@ import java.util.Random;
 public class Massive2 {
     public static void main (String[]args){
         int numbers[] = new int[12];
-        int max = numbers[0];
+        int min = numbers[0];
         int index = 0;
 
         Random randomGanerator = new Random();
         for (int i = 0; i < numbers.length; i++){
-            int randomInt = randomGanerator.nextInt(30) - 15;
+            int randomInt = randomGanerator.nextInt(8) - 15;
             numbers[i] = randomInt;
 
         }
         System.out.println(Arrays.toString(numbers));
 
-
         for (int i = 0; i < numbers.length; i++){
-            //if (max > numbers[i]){
-            //    System.out.println("You have only negative nambers!!!");
-            //    break;
-            //}
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        for (int i = 0; i < numbers.length; i++){
 
-            if (numbers[i] > max) {
-                max = numbers[i];
+            if (numbers[i] > min) {
+                min = numbers[i];
                 index = i;
             }
         }
-        System.out.println("Max number is " + "(" + max + ")" + " with index - " + "(" + index + ")");
+        System.out.println("Max number is " + "(" + min + ")" + " with index - " + "(" + index + ")");
     }
 }
