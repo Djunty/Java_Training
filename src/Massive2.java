f19913dd10bd25e7111797df048420db6fd8c8c0
Myza Taras <taras.myza@gmail.com>
2016-11-09 16:17:36 +0200

    Rundom massive (last max number)

--- src/Massive2.java
+++ src/Massive2.java
@@ -4,30 +4,26 @@ import java.util.Random;
 public class Massive2 {
     public static void main (String[]args){
         int numbers[] = new int[12];
-        int min = numbers[0];
         int index = 0;
 
         Random randomGanerator = new Random();
         for (int i = 0; i < numbers.length; i++){
-            int randomInt = randomGanerator.nextInt(8) - 15;
+            int randomInt = randomGanerator.nextInt(30) - 15;
             numbers[i] = randomInt;
 
         }
+        int max = numbers[0];
+
         System.out.println(Arrays.toString(numbers));
 
         for (int i = 0; i < numbers.length; i++){
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
-        }
-        for (int i = 0; i < numbers.length; i++){
 
-            if (numbers[i] > min) {
-                min = numbers[i];
+            if (numbers[i] >= max) {
+                max = numbers[i];
                 index = i;
             }
         }
-        System.out.println("Max number is " + "(" + min + ")" + " with index - " + "(" + index + ")");
+        System.out.println("Max number is " + "(" + max + ")" + " with index - " + "(" + index + ")");
 
     }
 }
