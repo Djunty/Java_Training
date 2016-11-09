b653b5ae7130327fddfb6fb04470bcae01d1924e
Myza Taras <taras.myza@gmail.com>
2016-11-09 21:52:30 +0200

    Rundom massive with chars (Partly done_1)

--- src/com/company/CharMassive.java
+++ src/com/company/CharMassive.java
@@ -6,14 +6,18 @@ import java.util.Random;
 public class CharMassive {
     public static void main (String []args){
         char letters[] = new char[100];
-        char a = 'a';
-
+        char a = 'A';
         Random randomGenerator = new Random();
         for (int i = 0; i < letters.length; i++){
             int randomInt = randomGenerator.nextInt(26);
-            letters[i] = (char) (a + randomInt);
-        }
-
+            letters[i] = (char) (a + randomInt);}
         System.out.println(Arrays.toString(letters));
+
+        for (int i = 0; i < letters.length; i++){
+            if (letters[i] == a) {
+                int index = i;
+                System.out.println(index);
+            }
+        }
     }
 }
