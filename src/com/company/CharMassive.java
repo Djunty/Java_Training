47d85c57d5ee37eea84cc56d0100c226b148116a
Myza Taras <taras.myza@gmail.com>
2016-11-11 00:47:19 +0200

    Rundom massive with chars (DONE)

--- src/com/company/CharMassive.java
+++ src/com/company/CharMassive.java
@@ -1,23 +1,47 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class CharMassive {
+    private static char vowels[] = {'A', 'E', 'I', 'O', 'U', 'Y'};
+    private static char letters[] = new char[100];
+    private static final char START_CHAR = 'A';
+
     public static void main (String []args){
-        char letters[] = new char[100];
-        char a = 'A';
+        charMassiveGeneration();
+        System.out.println("MAX Current cluster: " + (findMaxClusterSize() + 1));
+    }
+
+    private static int findMaxClusterSize() {
+        int currentMaxClusterSize = 0;
+        int currentClusterSize = 0;
+
+        for (int i = 0; i < letters.length; i++){
+            if (isVovel(letters[i])) {
+                if (currentClusterSize > currentMaxClusterSize) {
+                    currentMaxClusterSize = currentClusterSize;
+                }
+                currentClusterSize = 0;
+            } else {
+                currentClusterSize++;
+            }
+        }
+        return currentMaxClusterSize;
+    }
+
+    private static boolean isVovel(char a) {
+        for (int i = 0; i < vowels.length; i++) {
+            if (vowels[i] == a) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void charMassiveGeneration() {
         Random randomGenerator = new Random();
         for (int i = 0; i < letters.length; i++){
             int randomInt = randomGenerator.nextInt(26);
-            letters[i] = (char) (a + randomInt);}
-        System.out.println(Arrays.toString(letters));
-
-        for (int i = 0; i < letters.length; i++){
-            if (letters[i] == a) {
-                int index = i;
-                System.out.println(index);
-            }
-        }
+            letters[i] = (char) (START_CHAR + randomInt);}
     }
 }
